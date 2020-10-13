cf6dc3ade60986f71a33b4fa4d011fce04c56821
iclapcheeks <51503087+iclapcheeks@users.noreply.github.com>
2020-10-13 03:37:39 -0700

    Add files via upload

---
+++ nested.py
@@ -0,0 +1,16 @@
+def json_iterative(data, key):
+    results = []
+    items = [data]
+
+    while items:
+        current = items.pop(0)
+
+        if isinstance(current, dict):
+            if key in current:
+                results.append(current[key])
+            items.extend(current.values())
+
+        elif isinstance(current, list):
+            items.extend(current)
+
+    return results
---
+++ usage.py
@@ -0,0 +1,5 @@
+from nested import json_iterative
+
+data = {"rows":[{"elements":[{"distance":{"text":"227 mi","value":365468},"duration":{"text":"3 hours 54 mins","value":14064},"status":"OK"},{"distance":{"text":"94.6 mi","value":152193},"duration":{"text":"1 hour 44 mins","value":6227},"status":"OK"}]}]}
+parsed_data = json_iterative(data, 'text') #Replace "text" with the key that you are trying to parse
+print(parsed_data)
